259f3d685ffb6c5c18b7a98846a7d596b054fa51
Jan Eberhardt <eberhardt@math.tu-berlin.de>
2021-12-02 17:20:01 +0000

    Userid speichern
    	geändert:       db/install.xml
    	geändert:       lib.php

--- lib.php
+++ lib.php
@@ -86,15 +86,18 @@ function get_method_options($module) {
  * @throws dml_exception
  */
 function local_assessment_methods_coursemodule_edit_post_actions($data, $course) {
-    global $DB;
+    global $DB, $USER;
 
+    // if no login is present, use guest user
+    $userid = (is_object($USER) && !empty($USER->id)) ? $USER->id : 1;
     if ($record = $DB->get_record('assessment_methods', ['cmid' => $data->coursemodule])) {
         $record->method = $data->assessment_method;
+        $record->userid = $userid;
         $DB->update_record('assessment_methods', $record);
     } else {
         $DB->insert_record(
             'assessment_methods',
-            ['cmid' => $data->coursemodule, 'method' => $data->assessment_method]
+            ['cmid' => $data->coursemodule, 'userid' => $userid, 'method' => $data->assessment_method]
         );
     }
 }
