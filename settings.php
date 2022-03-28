12f5671f7831cdaf7f57a67de70a67f3360ed7b3
Martin Gauk <gauk@math.tu-berlin.de>
2022-03-28 17:00:21 +0200

    Check hassiteconfig

--- settings.php
+++ settings.php
@@ -1,30 +1,33 @@
 <?php
 
 //TODO: Make usable activities selectable
+if ($hassiteconfig) {
 
-/** @var admin_category $ADMIN */
-$ADMIN->add('localplugins', new admin_category(
-    'assessmentmethods',
-    get_string('pluginname', 'local_assessment_methods')
-));
+    /** @var admin_category $ADMIN */
+    $ADMIN->add('localplugins', new admin_category(
+        'assessmentmethods',
+        get_string('pluginname', 'local_assessment_methods')
+    ));
 
-$ADMIN->add('assessmentmethods', new admin_externalpage(
-    'assessmentmethods_admin',
-    get_string('assessment_method_list', 'local_assessment_methods'),
-    \local_assessment_methods\helper::get_admin_setting_url(),
-    'local/assessment_methods:manage'
-));
+    $ADMIN->add('assessmentmethods', new admin_externalpage(
+        'assessmentmethods_admin',
+        get_string('assessment_method_list', 'local_assessment_methods'),
+        \local_assessment_methods\helper::get_admin_setting_url(),
+        'local/assessment_methods:manage'
+    ));
 
-$ADMIN->add('assessmentmethods', new admin_externalpage(
-    'assessmentmethods_create',
-    get_string('create_method', 'local_assessment_methods'),
-    \local_assessment_methods\helper::get_method_edit_url(),
-    'local/assessment_methods:manage'
-));
+    $ADMIN->add('assessmentmethods', new admin_externalpage(
+        'assessmentmethods_create',
+        get_string('create_method', 'local_assessment_methods'),
+        \local_assessment_methods\helper::get_method_edit_url(),
+        'local/assessment_methods:manage'
+    ));
 
-$ADMIN->add('reports', new admin_externalpage(
-    'assessmentmethods_report',
-    get_string('pluginname', 'local_assessment_methods'),
-    \local_assessment_methods\helper::get_report_url(),
-    'local/assessment_methods:view_report'
-));
+    $ADMIN->add('reports', new admin_externalpage(
+        'assessmentmethods_report',
+        get_string('pluginname', 'local_assessment_methods'),
+        \local_assessment_methods\helper::get_report_url(),
+        'local/assessment_methods:view_report'
+    ));
+
+}
